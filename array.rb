ac6ff75fa32824b7227006f05eed525a8e804207
Erika <erika@istvan.life>
2020-10-28 21:38:43 +0200

    add array.rb

---
+++ array.rb
@@ -0,0 +1,13 @@
+# hasznalj for cilust i valtozoval ha erdekel az elem pozioja 
+
+# 1. hozz letre egy 10 elemu tombot veletlen szamokkal 1-10 ig
+
+# 2. ird ki a tomb elemeit egyenkent, egy sorba, space-vel elvalasztva
+
+# 3. ird ki a tomb elemeinek osszeget es szamtani kozeparanyosat (2 tizedes pontossaggal)
+
+# 4. keresd meg a tomb legnagyobb elemet (es a legkisebb elemet egy ciklusban)
+
+# 5. keresd meg a tomb masodik legkisebb elemet es ird ki ot magat es h hanyadik helyen van 
+
+# 6. olvass be egy szamot es ird ki, hogy hanyadik helyen van a szam a tombben, ha nincs benne akkor ird ki h "nincs benne"
